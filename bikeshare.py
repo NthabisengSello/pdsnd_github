739f1946acfa20316bba02237f0cbc97dff49fe2
Nthabiseng Sello <nthabisengsello03@gmail.com>
2022-08-29 12:10:47 +0200

    refactor:Adding a print statement

--- bikeshare.py
+++ bikeshare.py
@@ -16,6 +16,7 @@ def get_filters():
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data!')
+    print("We have bikeshare data for chicago,new york city and Washington")
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
            cities = ['chicago', 'new york city', 'washington']
